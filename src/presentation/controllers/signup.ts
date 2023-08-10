df77f1752d3f6cda8f5bac22791c58e48bab23f4
João Matheus Tripoli <joao.tripoli.dev@gmail.com>
2023-08-10 09:25:42 -0300

    End of lesson 7

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,25 @@
+import { MissingParamError } from '../errors/missing-params-error';
+import { badRequest } from '../helpers/http-helper';
+import { Controller } from '../protocols/controller';
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
+export class SignUpController implements Controller {
+  handle(httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = [
+      'name',
+      'email',
+      'password',
+      'passwordConfirmation',
+    ];
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field])
+        return badRequest(new MissingParamError(field));
+    }
+
+    return {
+      statusCode: 200,
+      body: 'okay',
+    };
+  }
+}
